8cdc94711019fce503416509f4d89cbadf23d1aa
mulhouse <41421248+mulhouse@users.noreply.github.com>
2026-02-04 11:20:55 +0100

    Delete docs/server directory

--- docs/server/assets/_tanstack-start-injected-head-scripts_v-BhzX7Z6T.js
+++
@@ -1,2 +0,0 @@
-const injectedHeadScripts = void 0;
-export { injectedHeadScripts };
--- docs/server/assets/start-gn-wP8i_.js
+++
@@ -1,4 +0,0 @@
-const startInstance = void 0;
-export { startInstance };
-
-//# sourceMappingURL=start-gn-wP8i_.js.map
