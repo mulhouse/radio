d30dd69928bed16d22e2d1637135927d1289e9e4
mulhouse <41421248+mulhouse@users.noreply.github.com>
2026-02-08 10:07:26 +0100

    Add additional assets to service worker cache

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,7 +1,16 @@
 const CACHE_NAME = 'radio-pwa-v1';
 const ASSETS = [
   './', './index.html', './styles.css', './app.js', './stations.json', './manifest.json',
-  './assets/logos/placeholder.svg'
+  './assets/logos/placeholder.svg',
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/France_Inter_logo_2021.svg/1200px-France_Inter_logo_2021.svg.png',
+  'https://duckduckgo.com/i/035a5e15d396c67a.png',
+  'https://www.francebleu.fr/client/immutable/assets/logo-ici-small.DwiLDMIK.svg',
+  'https://upload.wikimedia.org/wikipedia/fr/thumb/2/22/France_Musique_-_2008.svg/1024px-France_Musique_-_2008.svg.png',
+  'https://upload.wikimedia.org/wikipedia/fr/thumb/c/c9/France_Culture_-_2008.svg/1024px-France_Culture_-_2008.svg.png',
+  'https://www.rts.ch/hbv7/static/images/logos/logo_rts-rouge.svg',
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/FIP_logo_2021.svg/250px-FIP_logo_2021.svg.png',
+  'https://www.grrif.ch/wp-content/themes/grrif/img/player/logo-grrif.png',
+  'https://myradioendirect.fr/public/uploads/radio_img/nostalgie/play_250_250.webp'
 ];
 
 self.addEventListener('install', (evt)=>{
@@ -35,6 +44,7 @@ self.addEventListener('fetch', (evt)=>{
   })));
 
 });
+
 
 
 
